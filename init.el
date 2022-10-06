57e744bd09f96c8ae2530f07df0e33874e93343c
Muqiu Han <muqiu-han@outlook.com>
2022-10-06 15:14:19 +0800

    INIT

---
+++ init.el
@@ -0,0 +1,364 @@
+(require 'package)
+(require 'cl-lib)
+
+(setq url-proxy-services
+      '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+	("http" . "127.0.0.1:7890")
+	("https" . "127.0.0.1:7890")))
+
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+	(if (boundp 'package-selected-packages)
+            ;; Record this as a package the user installed explicitly
+            (package-install package nil)
+          (package-install package))
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(defun maybe-require-package (package &optional min-version no-refresh)
+  "Try to install PACKAGE, and return non-nil if successful.
+In the event of failure, return nil and print a warning message.
+Optionally require MIN-VERSION.  If NO-REFRESH is non-nil, the
+available package lists will not be re-downloaded in order to
+locate PACKAGE."
+  (condition-case err
+      (require-package package min-version no-refresh)
+    (error
+     (message "Couldn't install optional package `%s': %S" package err)
+     nil)))
+
+(require-package 'use-package)
+(require-package 'magit)
+(require-package 'prescient)
+(require-package 'treemacs-persp)
+(require-package 'treemacs-projectile)
+(require-package 'treemacs-magit)
+(require-package 'treemacs-all-the-icons)
+(require-package 'nano-theme)
+(require-package 'nano-modeline)
+(require-package 'doom-themes)
+(require-package 'window-numbering)
+(require-package 'rainbow-delimiters)
+(require-package 'racket-mode)
+(require-package 'merlin)
+(require-package 'ocamlformat)
+(require-package 'dune)
+(require-package 'ocp-indent)
+(require-package 'tuareg)
+(require-package 'yasnippet)
+(require-package 'hide-mode-line)
+(require-package 'markdown-mode)
+
+(defun icon-displayable-p ()
+  "Return non-nil if icons are displayable."
+  (and t
+       (or (display-graphic-p) (daemonp))
+       (or (featurep 'all-the-icons)
+           (require 'all-the-icons nil t))))
+
+(use-package treemacs
+  :commands (treemacs-follow-mode
+             treemacs-filewatch-mode
+             treemacs-fringe-indicator-mode
+             treemacs-git-mode)
+  :custom-face
+  (cfrs-border-color ((t (:background ,(face-foreground 'font-lock-comment-face nil t)))))
+  :bind (([f8]        . treemacs)
+         ("M-1"       . treemacs-select-window)
+         ("C-x 1"     . treemacs-delete-other-windows)
+         ("C-x t 1"   . treemacs-delete-other-windows)
+         ("C-x t t"   . treemacs)
+         ("C-x t b"   . treemacs-bookmark)
+         ("C-x t C-t" . treemacs-find-file)
+         ("C-x t M-t" . treemacs-find-tag)
+         :map treemacs-mode-map
+         ([mouse-1]   . treemacs-single-click-expand-action))
+  :config
+  (setq treemacs-collapse-dirs           (if treemacs-python-executable 3 0)
+        treemacs-missing-project-action  'remove
+        treemacs-sorting                 'alphabetic-asc
+        treemacs-follow-after-init       t
+        treemacs-width                   50
+        treemacs-no-png-images           nil)
+  :config
+  (treemacs-follow-mode t)
+  (treemacs-filewatch-mode t)
+  (pcase (cons (not (null (executable-find "git")))
+               (not (null (executable-find "python3"))))
+    (`(t . t)
+     (treemacs-git-mode 'deferred))
+    (`(t . _)
+     (treemacs-git-mode 'simple)))
+
+  (use-package treemacs-projectile
+    :after projectile
+    :bind (:map projectile-command-map
+		("h" . treemacs-projectile)))
+
+  (use-package treemacs-magit
+    :after magit
+    :commands treemacs-magit--schedule-update
+    :hook ((magit-post-commit
+            git-commit-post-finish
+            magit-post-stage
+            magit-post-unstage)
+           . treemacs-magit--schedule-update))
+
+  (use-package treemacs-persp
+    :after persp-mode
+    :demand t
+    :functions treemacs-set-scope-type
+    :config (treemacs-set-scope-type 'Perspectives))
+
+  (use-package treemacs-all-the-icons
+    :init
+    (require 'treemacs-all-the-icons)
+    (treemacs-load-theme "all-the-icons")))
+
+(use-package lambda-line
+  :straight (:type git :host github :repo "lambda-emacs/lambda-line") 
+  :custom
+  (lambda-line-icon-time t) ;; requires ClockFace font (see below)
+  (lambda-line-clockface-update-fontset "ClockFaceRect") ;; set clock icon
+  (lambda-line-position 'top) ;; Set position of status-line 
+  (lambda-line-abbrev t) ;; abbreviate major modes
+  (lambda-line-hspace "  ")  ;; add some cushion
+  (lambda-line-prefix t) ;; use a prefix symbol
+  (lambda-line-prefix-padding nil) ;; no extra space for prefix 
+  (lambda-line-status-invert nil)  ;; no invert colors
+  (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
+  (lambda-line-gui-mod-symbol " ⬤") 
+  (lambda-line-gui-rw-symbol  " ◯") 
+  (lambda-line-space-top +.50)  ;; padding on top and bottom of line
+  (lambda-line-space-bottom -.50)
+  (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
+  :config
+   ;; activate lambda-line
+   (lambda-line-mode)
+   ;; set divider line in footer
+   (when (eq lambda-line-position 'top)
+     (setq-default mode-line-format (list "%_"))
+     (setq mode-line-format (list "%_"))))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+
+(use-package racket-mode
+  :hook (racket-mode . racket-xp-mode))
+
+(use-package tuareg
+  :config
+  (use-package merlin
+    :hook (tuareg-mode . merlin-mode))
+
+  (use-package ocamlformat
+    :hook (tuareg-mode . ocamlformat-before-save))
+
+  (use-package ocp-indent
+    :hook (tuareg-mode . ocp-indent-mode)))
+
+
+(use-package lsp-bridge
+  :load-path "~/.emacs.d/site-lisp/lsp-bridge"
+  :init
+  (require 'yasnippet)
+  (yas-global-mode 1)
+
+  (require 'lsp-bridge)
+  (global-lsp-bridge-mode))
+
+(fringe-mode 0)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(toggle-frame-maximized)
+
+(set-face-attribute 'default nil :font "SF Mono" :height 130 :weight 'Semibold)
+
+(load-theme 'doom-material-dark t)
+
+(use-package hide-mode-line
+  :hook (after-init . global-hide-mode-line-mode))
+
+(use-package window-numbering
+  :hook (after-init . window-numbering-mode))
+
+(use-package display-line-numbers
+  :ensure nil
+  :hook ((prog-mode yaml-mode conf-mode) . display-line-numbers-mode)
+  :init (setq display-line-numbers-width-start t)
+  :config
+  (set-face-attribute 'line-number nil
+		    :font (face-attribute 'default :font)
+		    :height (face-attribute 'default :height)
+		    :weight (face-attribute 'default :weight))
+  
+  (set-face-attribute 'line-number-current-line nil
+		    :font (face-attribute 'default :font)
+		    :height (face-attribute 'default :height)
+		    :weight (face-attribute 'default :weight)))
+
+(defun splash-screen ()
+  "Emacs splash screen"
+  
+  (interactive)
+  (let* ((splash-buffer  (get-buffer-create "*splash*"))
+         (recover-session (and auto-save-list-file-prefix
+                               (file-directory-p (file-name-directory
+                                                  auto-save-list-file-prefix))))
+         (height         (- (window-body-height nil) 1))
+         (width          (window-body-width nil))
+         (padding-center (- (/ height 2) 1))
+         (padding-bottom (- height (/ height 2) 3)))
+
+    ;; If there are buffer associated with filenames,
+    ;;  we don't show splash screen.
+    (if (eq 0 (length (cl-loop for buf in (buffer-list)
+                              if (buffer-file-name buf)
+                              collect (buffer-file-name buf))))
+        
+        (with-current-buffer splash-buffer
+          (erase-buffer)
+          
+          ;; Buffer local settings
+          (if (one-window-p)
+              (setq mode-line-format nil))
+          (setq cursor-type nil)
+          (setq vertical-scroll-bar nil)
+          (setq horizontal-scroll-bar nil)
+          (setq fill-column width)
+          (face-remap-add-relative 'link :underline nil)
+
+          ;; Vertical padding to center
+          (insert-char ?\n padding-center)
+
+          ;; Central text
+          (insert-text-button " www.gnu.org "
+                     'action (lambda (_) (browse-url "https://www.gnu.org"))
+                     'help-echo "Visit www.gnu.org website"
+                     'follow-link t)
+          (center-line) (insert "\n")
+          (insert (concat
+                   (propertize "GNU Emacs"  'face 'bold)
+                   " " "version "
+                   (format "%d.%d" emacs-major-version emacs-minor-version)))
+          (center-line) (insert "\n")
+          (insert (propertize "A free/libre editor" 'face 'shadow))
+          (center-line)
+
+
+          ;; Vertical padding to bottom
+          (insert-char ?\n padding-bottom)
+
+          ;; Recover session button
+          (when recover-session
+            (delete-char -2)
+            (insert-text-button " [Recover session] "
+                 'action (lambda (_) (call-interactively 'recover-session))
+                   'help-echo "Recover previous session"
+                   'face 'warning
+                   'follow-link t)
+            (center-line) (insert "\n") (insert "\n"))
+
+          ;; Copyright text
+          (insert (propertize
+                   "GNU Emacs comes with ABSOLUTELY NO WARRANTY" 'face 'shadow))
+          (center-line) (insert "\n")
+          (insert (propertize
+                   "Copyright (C) 2020 Free Software Foundation, Inc." 'face 'shadow))
+          (center-line) (insert "\n")
+
+          (goto-char 0)
+          (read-only-mode t)
+          
+          (local-set-key [t]               'splash-screen-fade-to-about)
+          (local-set-key (kbd "C-[")       'splash-screen-fade-to-default)
+          (local-set-key (kbd "<escape>")  'splash-screen-fade-to-default)
+          (local-set-key (kbd "q")         'splash-screen-fade-to-default)
+          (local-set-key (kbd "<mouse-1>") 'mouse-set-point)
+          (local-set-key (kbd "<mouse-2>") 'operate-this-button)
+          ;; (local-set-key " "               'splash-screen-fade-to-default)
+          ;; (local-set-key "x"               'splash-screen-fade-to-default)
+          ;; (local-set-key (kbd "<RET>")     'splash-screen-fade-to-default)
+          ;; (local-set-key (kbd "<return>")  'splash-screen-fade-to-default)
+          (display-buffer-same-window splash-buffer nil)
+          (run-with-idle-timer 10.0 nil    'splash-screen-fade-to-about)))))
+
+
+;; Mac animation, only available from
+;;  https://bitbucket.org/mituharu/emacs-mac/src/master/
+;;  https://github.com/railwaycat/homebrew-emacsmacport
+(defvar mac-animation-locked-p nil)
+(defun mac-animation-toggle-lock ()
+  (setq mac-animation-locked-p (not mac-animation-locked-p)))
+(defun mac-animation-fade-out (duration &rest args)
+  (unless mac-animation-locked-p
+    (mac-animation-toggle-lock)
+    (mac-start-animation nil :type 'fade-out :duration duration)
+    (run-with-timer duration nil 'mac-animation-toggle-lock)))
+
+(defun splash-screen-fade-to (about duration)
+  "Fade out current frame for duration and goes to command-or-bufffer"
+  (interactive)
+  (defalias 'mac-animation-fade-out-local
+    (apply-partially 'mac-animation-fade-out duration))
+  (if (get-buffer "*splash*")
+      (progn (if (and (display-graphic-p) (fboundp 'mac-start-animation))
+                 (advice-add 'set-window-buffer
+                             :before 'mac-animation-fade-out-local))
+             (if about (about-emacs))
+             (kill-buffer "*splash*")
+             (if (and (display-graphic-p) (fboundp 'mac-start-animation))
+                 (advice-remove 'set-window-buffer
+                                'mac-animation-fade-out-local)))))
+(defun splash-screen-fade-to-about ()
+  (interactive) (splash-screen-fade-to 1 1.0))
+(defun splash-screen-fade-to-default ()
+  (interactive) (splash-screen-fade-to nil 0.25))
+
+(defun splash-screen-kill ()
+  "Kill the splash screen buffer (immediately)."
+  (interactive)
+  (if (get-buffer "*splash*")
+        (kill-buffer "*splash*")))
+
+;; Suppress any startup message in the echo area
+(run-with-idle-timer 0.05 nil (lambda() (message nil)))
+
+;; Install hook after frame parameters have been applied and only if
+;; no option on the command line
+(if (and (not (member "-no-splash"  command-line-args))
+         (not (member "--file"      command-line-args))
+         (not (member "--insert"    command-line-args))
+         (not (member "--find-file" command-line-args))
+         (not inhibit-startup-screen)
+         )
+    (progn
+      (add-hook 'window-setup-hook 'splash-screen)
+      (setq inhibit-startup-screen t 
+            inhibit-startup-message t
+            inhibit-startup-echo-area-message t)))
