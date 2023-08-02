b7b384b696c895f434d83a83b2d3e121624f51e1
Muqiu Han <muqiu-han@outlook.com>
2023-08-02 09:05:40 +0800

    More sophisticated pixel flip support

--- init.el
+++ init.el
@@ -348,8 +348,20 @@
   :hook (after-init . pixel-scroll-precision-mode)
   :config
   (setq pixel-scroll-precision-interpolate-page t)
-  (defalias 'scroll-up-command 'pixel-scroll-interpolate-down)
-  (defalias 'scroll-down-command 'pixel-scroll-interpolate-up))
+  (defun +pixel-scroll-interpolate-down (&optional lines)
+    (interactive)
+    (if lines
+	(pixel-scroll-precision-interpolate (* -1 lines (pixel-line-height)))
+      (pixel-scroll-interpolate-down)))
+
+  (defun +pixel-scroll-interpolate-up (&optional lines)
+    (interactive)
+    (if lines
+	(pixel-scroll-precision-interpolate (* lines (pixel-line-height))))
+    (pixel-scroll-interpolate-up))
+
+  (defalias 'scroll-up-command '+pixel-scroll-interpolate-down)
+  (defalias 'scroll-down-command '+pixel-scroll-interpolate-up))
 
 ;; Which key
 (use-package which-key
